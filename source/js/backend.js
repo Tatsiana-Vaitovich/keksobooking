2aca45a6cda4978449fc3726fd4d108153d88e0e
Tatsiana <t_a_b@tut.by>
2020-11-11 09:32:33 +0300

    added backend() in window.backend

--- source/js/backend.js
+++ source/js/backend.js
@@ -18,6 +18,38 @@
     "SERVER_ERROR": 500,
   };
 
+  function onXhrGetResponseForLoad(onLoad) {
+    switch (this.status) {
+      case (Code.SUCCESS):
+        onLoad(this.response);
+        break;
+      case (Code.CACHED):
+        error = "неверный запрос";
+        break;
+      case (Code.NOT_FOUND_ERROR):
+        error = "ничего не найдено";
+        break;
+      default:
+        error("Статус ответа: " + xhr.status + " xhr.statusText");
+    }
+    if (error) {
+      onError(error);
+    }
+  }
+
+  function onXhrGetResponseForUpload(onLoad) {
+    switch (this.status) {
+      case (Code.SUCCESS):
+        onLoad();
+        break;
+      default:
+        error = ("Извините, произошла ошибка,<br>Ваше объявление не удалось опубликовать");
+    }
+    if (error) {
+      onError(error);
+    }
+  }
+
   // загрузка данных на сервер
 
   function upload(data, onLoad, onError) {
@@ -96,9 +128,50 @@
     xhr.send();
   }
 
+  function backend(onLoad, onError, metod) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.timeout = MAX_WAITING_TIME_RESPONSE;
+    let error;
+    switch (metod) {
+      case ("GET"):
+        xhr.addEventListener("load", function() {
+          onXhrGetResponseForLoad(onLoad);
+        });
+        xhr.addEventListener("error", function() {
+          error = "Произошла ошибка соединения";
+          onError(error);
+          try {
+            window.backupMethodForLoadingData.useJSONP();
+          } catch (err) {
+            console.log(err.message);
+            window.backupMethodForLoadingData.useMock();
+          }
+        });
+        xhr.addEventListener("timeout", function() {
+          error = "Запрос не успел выполниться за " + xhr.timeout + "мс";
+          onError(error);
+        });
+        xhr.open("GET", URL_GET);
+        xhr.send();
+        break;
+      case ("POST"):
+        xhr.addEventListener("load", function() {
+          onXhrGetResponseForUpload(onLoad);
+        });
+        xhr.addEventListener("error", function() {
+          error = "Не удалось загрузить форму.<br>Произошла ошибка соединения";
+          onError(error);
+        });
+        xhr.open("POST", URL_POST);
+        xhr.send(data);
+    }
+  }
+
   window.backend = {
     "upload": upload,
     "load": load,
+    "backend": backend,
     "URL_GET": URL_GET,
   };
 
